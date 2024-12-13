7b4fa8c8ac4486db8de9a0f5b5b66045024ce53e
larry123 <larioslarry8@gmail.com>
2024-12-13 10:42:51 -0600

    header almost finished

--- hatch_larry/src/components/HelloWorld.vue
+++ hatch_larry/src/components/HelloWorld.vue
@@ -17,14 +17,24 @@ export default defineComponent({
 
     <div class="header">
 
-      <div class="egg_container">
+      <div class="nav_bar">
         <button class="egg">egg</button>
-        Hatch
+        <p Class="HatchWord">Hatch</p>
         <select name="version" id="selversion">
             <option value="opt1">1.0</option>
             <option value="opt1">1.1</option>
         </select>
       
+      </div>
+
+
+      <div class="nav_bar_header_2">
+        <button class="sun_btn">sun</button>
+        <input type="text" class="search-input" placeholder="🔎 Buscar">
+        <a href="#">pypa/Hatch</a>
+
+
+
       </div>
 
 
@@ -77,6 +87,7 @@ export default defineComponent({
   grid-column-start: 2;
   background-color: #4051B5;
   grid-template-rows: 1fr 1fr;
+  grid-template-columns: 698px 621px;
 
 
 }
@@ -88,28 +99,86 @@ justify-content: flex-start;
 font-size: 15px;
 margin: 20px;
 grid-row-start: 2;
+
 }
 
 
 .links a{
+  text-decoration: none;
   margin: 0 10px;
   color: white;
 }
 
-.egg_container{
+.nav_bar{
   display: flex;
+  align-items: center;
+  gap: 10px;
   grid-row-start: 1;
+  color: white;
+  margin: 20px;
+  height: 10px;
+}
+
+
+.egg{
+  display: inline-block;
+  margin: 10px;
+  height: 30px;
+  width: 30px;
+  margin-left: 10px;
+  background-color: #4051B5;
+  border: #4051B5;
+  color: white;
+}
+
+#selversion{
+display: inline-block;
+margin:10px;
+height: 30px;
+width: 60px;
+background-color: #4051B5;
+border: #4051B5;
+color: white;
+}
+.HatchWord{
+  display: inline-block;
+  margin: 10px;
+  
+}
+
+.nav_bar_header_2{
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  grid-row-start: 1;
+  grid-column-start: 2;
+  color: white;
   margin: 20px;
   height: 10px;
 
 }
+.nav_bar_header_2 a{
+  text-decoration: none;
+  margin: 0 10px;
+  color: white;
+}
 
-.egg{
+.sun_btn{
+  display: inline-block;
+  margin: 10px;
   height: 30px;
   width: 30px;
   margin-left: 10px;
+  background-color: #4051B5;
+  border: #4051B5;
+  color: white;
 }
 
+.search-input{
+  background-color: #5766BE;
+  color: white;
+  border: 0;
+}
 
 
 

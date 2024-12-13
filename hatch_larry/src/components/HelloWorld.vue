5cca5130cb20408d99d77b039235cbe7db3e221a
larry123 <larioslarry8@gmail.com>
2024-12-12 22:19:31 -0600

    putting some part of the header

--- hatch_larry/src/components/HelloWorld.vue
+++ hatch_larry/src/components/HelloWorld.vue
@@ -1,41 +1,118 @@
-<script setup lang="ts">
-defineProps<{
-  msg: string
-}>()
+<script lang="ts">
+
+
+import { defineComponent} from "vue";
+
+
+export default defineComponent({
+    name: "HatchEmulation"
+    
+})
+
 </script>
 
+
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
+  <div class="main">
+
+    <div class="header">
+
+      <div class="egg_container">
+        <button class="egg">egg</button>
+        Hatch
+        <select name="version" id="selversion">
+            <option value="opt1">1.0</option>
+            <option value="opt1">1.1</option>
+        </select>
+      
+      </div>
+
+
+
+      <div class="links">
+        <a href="#">Home</a>
+        <a href="#">configuration</a>
+        <a href="#">CLI</a>
+        <a href="#">Plugins</a>
+        <a href="#">How-to</a>
+        <a href="#">Tutorials</a>
+        <a href="#">Meta</a>
+        <a href="#">Blogs</a>
+
+
+      </div>
+
+
+
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
   </div>
 </template>
 
 <style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
+.main{
+  height: 100vh;
+  width: 100vw;
+  display: grid;
+  grid-template-rows: 100px 1fr 70px;
+  grid-template-columns: 270px 1fr 270px  ;
+  
 }
 
-h3 {
-  font-size: 1.2rem;
+.header{
+  margin: 0;
+  padding: 0;
+  display: grid;
+  grid-column-start: 2;
+  background-color: #4051B5;
+  grid-template-rows: 1fr 1fr;
+
+
 }
 
-.greetings h1,
-.greetings h3 {
-  text-align: center;
+.links {
+display: flex;
+align-items: flex-end;
+justify-content: flex-start;
+font-size: 15px;
+margin: 20px;
+grid-row-start: 2;
 }
 
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
+
+.links a{
+  margin: 0 10px;
+  color: white;
 }
+
+.egg_container{
+  display: flex;
+  grid-row-start: 1;
+  margin: 20px;
+  height: 10px;
+
+}
+
+.egg{
+  height: 30px;
+  width: 30px;
+  margin-left: 10px;
+}
+
+
+
+
 </style>
+
+
